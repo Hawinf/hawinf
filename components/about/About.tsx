d5c7fe795299b79b470879aed52cdc23f8161b71
Hawinf <hawinf54@gmail.com>
2023-10-25 01:41:27 +0400

    change haeder bg

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -2,15 +2,8 @@ import AboutStyle from './about.module.css';
 
 export const About = () => {
     return (
-        // <div className={AboutStyle['wrapper-about']}>
-        //     <h5>About</h5>
-        // </div>
-        <div className={AboutStyle["wrapper"]}>
-	<svg className={AboutStyle['svg']}>
-		<text x="50%" y="50%" dy=".35em" text-anchor="middle">
-			CSS Portal
-		</text>
-	</svg>
-</div>
+        <div className={AboutStyle['wrapper-about']}>
+            <h5>About</h5>
+        </div>
     )
 }

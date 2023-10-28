e68cda37fcfcaa76147cb940f6c87cbe3e56a328
Hawinf <hawinf54@gmail.com>
2023-10-28 22:44:50 +0400

    editing tools.tsx

--- components/Tools/Tool.tsx
+++ components/Tools/Tool.tsx
@@ -5,6 +5,17 @@ export const Tools = () => {
         <div className={ToolStyle['wrapper-tools']}>
             <div className='container'>
                 <h1 className={ToolStyle['tool-judul']}>Developed With</h1>
+                <div className={ToolStyle['tool-categ']}>
+                    <div className={ToolStyle['categ-1']}>
+                        <h6 className={ToolStyle['cate1judul']}>Tools</h6>
+                        <div className={ToolStyle['card-code']}>
+                            <p>VS Code</p>
+                        </div>
+                    </div>
+                    <div className={ToolStyle['categ-2']}>
+                        <h6 className={ToolStyle['cate2judul']}>Language</h6>
+                    </div>
+                </div>
             </div>
         </div>
     )

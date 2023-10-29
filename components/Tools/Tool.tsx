81b2a431a85fc263a64a49f0c000b8feff2539c2
Hawinf <hawinf54@gmail.com>
2023-10-29 19:52:25 +0400

    editing about

--- components/Tools/Tool.tsx
+++ components/Tools/Tool.tsx
@@ -12,8 +12,16 @@ export const Tools = () => {
                             <p>VS Code</p>
                         </div>
                     </div>
-                    <div className={ToolStyle['categ-2']}>
-                        <h6 className={ToolStyle['cate2judul']}>Language</h6>
+                    <div className={ToolStyle['categ-1']}>
+                        <h6 className={ToolStyle['cate1judul']}>Language</h6>
+                        <div className={ToolStyle['card-code-lang']}>
+                            <p>HTML 5</p>
+                            <p className={ToolStyle['css']}>CSS 3</p>
+                            <p className={ToolStyle['bootstrap']}>Bootstrap</p>
+                            <p className={ToolStyle['bootstrap']}>Javascript</p>
+                            <p>React JS</p>
+                            <p>Next JS</p>
+                        </div>
                     </div>
                 </div>
             </div>
--- components/about/About.tsx
+++ components/about/About.tsx
@@ -18,22 +18,34 @@ export const About = () => {
                     <div className='col-lg-4'>
                         <div className={AboutStyle['card-work-1']}>
                             <h1>Binar Car Rental</h1>
+                            <Link href=''>
+                                <p>Visit Web</p>
+                            </Link>
                         </div>
                     </div>
                     <div className='col-lg-4'>
                         <div className={AboutStyle['card-work-2']}>
                             <h1>Binar Car Rental Admin</h1>
+                            <Link href=''>
+                                <p>Visit Web</p>
+                            </Link>
                         </div>
                     </div>
                     <div className='col-lg-8'>
                         <div className={AboutStyle['card-work-3']}>
                             <h1>Landsnature</h1>
+                            <Link href=''>
+                                <p>Visit Web</p>
+                            </Link>
                         </div>
                     </div>
                     <div className='col-lg-4'>
                         <div className={AboutStyle['card-work-4']}>
                             <h1>Repository</h1>
-                            <p>Feel free to explore and review my project on GitHub. You'll find all the code, documentation, and any other materials related to the project, and I'd appreciate your feedback and contributions if you have any.</p>
+                            <p>Feel free to explore and review my project on GitHub. You'll find all the code, documentation.</p>
+                            <Link href=''>
+                                <p className={AboutStyle['ongithub']}>Github</p>
+                            </Link>
                         </div>
                     </div>
                 </div>
